e6772734bedcdaf5fc2e3f062646ae156353c71c
ewgerr <eewger@gmail.com>
2025-02-20 00:27:47 +0200

    add randon choise

--- game.js
+++ game.js
@@ -1,1 +1,10 @@
 console.log('game.js loaded');
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  console.log(getRandomInt(3));
+
+function getComputerChoice(){
+    
+}
